a6730af3993ea896031078f1fb89b3b938c81628
java2013b <java2013b@gmail.com>
2022-07-19 22:21:50 +0800

    Update crawl.sh

--- crawl.sh
+++ crawl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/asoul/taifex-crawler/data
+cd /home/java2013b/taifex-crawler-2022/data
 a=$(date +"%Y_%m_%d")
 wget https://www.taifex.com.tw/file/taifex/Dailydownload/Dailydownload/Daily_$a.zip
 cd ..
