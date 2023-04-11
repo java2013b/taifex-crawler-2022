cf904366626127ef65c3ba93b31e497929e20b6f
java2013b <java2013b@gmail.com>
2023-04-11 17:38:00 +0800

    Update check.sh

--- data/check.sh
+++ data/check.sh
@@ -1,2 +1,9 @@
 #find -type f -name 'Daily*' -size -1000c -delete;
 find -type f -name 'Daily*' -size -1000c -print0 | xargs -I {} -0 git rm "{}";
+cd ..
+git add .
+git commit -m "daily update"
+git push
+git fetch --depth=1
+git reflog expire --expire-unreachable=now --all
+git gc --aggressive --prune=all
