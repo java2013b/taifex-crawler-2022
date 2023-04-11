999b556dd5d7e5ebb80c97b18b8ebb28e82d79d4
java2013b <java2013b@gmail.com>
2023-04-11 17:28:55 +0800

    Update check.sh

--- data/check.sh
+++ data/check.sh
@@ -1,2 +1,2 @@
 #find -type f -name 'Daily*' -size -1000c -delete;
-find -type f -name 'Daily*' -size -1000c -print0 | xargs -I {} -0 /usr/bin/rm -rfv "{}";
+find -type f -name 'Daily*' -size -1000c -print0 | xargs -I {} -0 git rm -v "{}";
